272f35831d864d358a7cefbc16a42556bc9eeaea
git4won <846820279@qq.com>
2018-03-26 11:13:15 +0800

    默认关闭wget代理服务器选项

--- Shell/translate/baidu_dict.sh
+++ Shell/translate/baidu_dict.sh
@@ -6,7 +6,7 @@
 #   Author      : ZhengFuYuan
 #   Create Date : 2018-01-04
 #   Description : 使用百度词典。注意：只能将英语翻译成中文
-#                 依赖: wget
+#                 依赖: wget, sed, awk
 #                 用法: ./baidu_dict.sh keyword    支持英文句子，如下:
 #                       $ ./baidu_dict.sh I like you
 #                       我喜欢你
@@ -20,7 +20,8 @@ keyword=$@
 # 将参数中的空格替换成URL编码'%20'
 keyword=`echo $keyword | sed "s/ /%20/g"`
 url='http://dict.baidu.com/s?wd='$keyword'&f=3'
-wget_proxy='http_proxy=http://192.168.1.155:8888'
+# 代理服务器（可选）
+# wget_proxy='http_proxy=http://192.168.1.155:8888'
 
 # 避免这次wget失败而使用上次的结果
 rm /tmp/dict > /dev/null 2>&1
