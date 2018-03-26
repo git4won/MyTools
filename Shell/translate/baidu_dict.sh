158f8ab48301b82d2526accb6774aaf99145ba9c
Ubuntu-16.04.03-64bit <won@gospell.com>
2018-03-25 20:05:35 -0700

    add a tool written by shell

---
+++ Shell/translate/baidu_dict.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#coding=utf-8
+
+#================================================================
+#   File Name   : baidu_dict.sh
+#   Author      : ZhengFuYuan
+#   Create Date : 2018-01-04
+#   Description : 使用百度词典。注意：只能将英语翻译成中文
+#                 依赖: wget
+#                 用法: ./baidu_dict.sh keyword    支持英文句子，如下:
+#                       $ ./baidu_dict.sh I like you
+#                       我喜欢你
+#                 原理: 从返回的html结果中，从"<a href="?wd=我喜欢你">我喜欢你</a>"提取出'>'和'<'之间的内容
+#================================================================
+
+#set -v
+
+# 获取所有参数
+keyword=$@
+# 将参数中的空格替换成URL编码'%20'
+keyword=`echo $keyword | sed "s/ /%20/g"`
+url='http://dict.baidu.com/s?wd='$keyword'&f=3'
+wget_proxy='http_proxy=http://192.168.1.155:8888'
+
+# 避免这次wget失败而使用上次的结果
+rm /tmp/dict > /dev/null 2>&1
+
+wget $url -e $wget_proxy -O /tmp/dict -nv > /dev/null 2>&1; cat /tmp/dict | awk '/href=\"\?wd=/{print $2}' | awk -F '[<>]' '{print $2}'
+
